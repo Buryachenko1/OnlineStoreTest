fbe08667b141acc3fa959251d9346c75f3de9f41
Evgenyia Buryachenko <evgeniyaburyachenko@seznam.cz>
2024-07-14 16:51:13 +0200

    Initial commit

---
+++ src/main/java/org/example/OnlineStorePage.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.openqa.selenium.support.FindBy;
+
+import java.sql.Driver;
+
+
+public class OnlinesStorePage {
+    Driver driver;
+    //something to check
+@FindBy
